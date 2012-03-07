be474b0cdf4dfe07b606043bb87daaad1b0d4985
Walter Heck <walter@tribily.com>
2012-03-07 15:12:57 +0800

    Each include gets it's own line

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,5 @@
 class php($cli_ini_content = undef, $cli_ini_source = undef) {
-    include php::params, php::install, php::config
+    include php::params
+    include php::install
+    include php::config
 }
