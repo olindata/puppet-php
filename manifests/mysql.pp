7a02544e48862ca309cb92d83d40439d1dc02f77
Walter Heck <walter@tribily.com>
2012-03-08 16:43:49 +0800

    added php5-mysql files

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -4,4 +4,19 @@ class php::mysql {
     ensure => present
   }
 
+  file {'/etc/php5/conf.d/mysqli.ini':
+    ensure  => present,
+    source  => 'file:///php/mysqli.ini'
+  }
+
+  file {'/etc/php5/conf.d/mysql.ini':
+    ensure  => present,
+    source  => 'file:///php/mysqli.ini'
+  }
+
+  file {'/etc/php5/conf.d/pdo_mysql.ini':
+    ensure  => present,
+    source  => 'file:///php/pdo_mysql.ini'
+  }
+
 }
