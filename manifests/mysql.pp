8c5a74210348af1ea69200d9b55a668490a0b4f0
Walter Heck <walter@tribily.com>
2012-03-08 16:50:42 +0800

    correct file resource paths

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -6,17 +6,17 @@ class php::mysql {
 
   file {'/etc/php5/conf.d/mysqli.ini':
     ensure  => present,
-    source  => 'file:///php/mysqli.ini'
+    source  => 'puppet:///files/php/mysqli.ini'
   }
 
   file {'/etc/php5/conf.d/mysql.ini':
     ensure  => present,
-    source  => 'file:///php/mysqli.ini'
+    source  => 'puppet:///files/php/mysqli.ini'
   }
 
   file {'/etc/php5/conf.d/pdo_mysql.ini':
     ensure  => present,
-    source  => 'file:///php/pdo_mysql.ini'
+    source  => 'puppet:///files/php/pdo_mysql.ini'
   }
 
 }
